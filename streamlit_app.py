33ae38b4b7092f6517d40cdb1c72a7d4f2265bf4
Vaughn Fajardo <40432972+vaughnfajardo@users.noreply.github.com>
2024-05-27 14:33:36 +0800

    adjusted UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,9 +23,25 @@ index = PineconeVectorStore(index_name=index_name, embedding=embeddings)
 llm = ChatOpenAI(api_key=openai_api_key, model_name="gpt-4o", temperature=0.4)
 chain=load_qa_chain(llm,chain_type="stuff")
 
-if 'conversation' not in st.session_state:
-    st.session_state['conversation'] = []
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
+st.set_page_config(page_title="OBFchat", page_icon="💙")
+st.title("OBFchat 💙")
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+def process_input(user_input):
+    if user_input:
+        if user_input[-1].isalpha():
+            user_input += "."
+        response = generate_response(user_input)
+        st.session_state.conversation.append(f"You: {user_input}")
+        st.session_state.conversation.append(f"OBFchat: {response}")
+        st.experimental_rerun()
+        
 def retrieve_query(query, k=10):
     matching_results = index.similarity_search(query, k=k)
     return matching_results
@@ -41,28 +57,5 @@ def generate_response(text):
     answer = retrieve_answers(text)
     return answer
 
-# Function to simulate conversational response
-def process_input(user_input):
-    if user_input:
-        if user_input[-1].isalpha():
-            user_input += "."
-        response = generate_response(user_input)
-        st.session_state.conversation.append(f"You: {user_input}")
-        st.session_state.conversation.append(f"OBFchat: {response}")
-        st.experimental_rerun()
-
-# Streamlit UI setup
-st.set_page_config(page_title="OBFchat", page_icon="💙")
-st.title("OBFchat 💙")
-
-for author, line in st.session_state.conversation:
-    with st.chat_message(author if author == "You" else "assistant"):
-        st.write(line)
-
-# Input and button for new messages
-user_input = st.text_input("Ask a question:", key="user_input", on_change=None)
-
-if st.button('Send'):
-    process_input(user_input)
-    st.session_state.user_input = ""  # Clear input after processing
-    st.experimental_rerun()
+if prompt := st.chat_input("Ask a question:"):
+    process_input(prompt)
